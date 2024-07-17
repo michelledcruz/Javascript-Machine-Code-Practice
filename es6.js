6a8947b7e643f4fdb82ef4a42907f3e87d4d8166
Michelle Dcruz <52381792+michelledcruz@users.noreply.github.com>
2024-07-17 13:08:35 +1000

    Update es6.js

--- es6.js
+++ es6.js
@@ -1,1 +1,14 @@
+//Given a string reverse each word in the sentence
 
+var str = "hey how are you";
+
+var split_word = str.split(" ") // split word first
+var arr = split_word.map(function(word){
+  return word.split("").reverse().join(""); // loop through each word and reverse
+});
+
+var res = arr.join(" ") // join all words by space
+console.log(res);
+
+
+//How to check if the given object is array or not
