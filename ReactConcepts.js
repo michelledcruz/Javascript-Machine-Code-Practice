848e260cac67e4fa6e76d220d0cf331f5d0d8f54
Michelle Dcruz <52381792+michelledcruz@users.noreply.github.com>
2024-07-21 16:35:17 +1000

    Update ReactConcepts.js

--- ReactConcepts.js
+++ ReactConcepts.js
@@ -1,1 +1,26 @@
+//How can you use the useState hook in React to manage the state of a text input field and display the entered text below the input field as the user types?
+import { useState } from 'react';
+
+export default function MyApp() {
+  return (
+    <div>
+      <p>Enter text below</p>
+      <FormInput/>
+    </div>
+  );
+}
+
+function FormInput() {
+  const [searchText, setSearch ] = useState("React question");
+
+  
+  return (
+    <>
+      <input type="text" id="name" value={searchText} onChange={(e) => {
+      setSearch(e.target.value);
+      }}></input>
+      <p>{searchText}</p>
+    </>
+  );
+}
 
